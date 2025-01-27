c6f48819b95c5b8b8832800b4ac3a5fcf156ffbd
Artem <kok321416x@yandex.ru>
2025-01-27 19:48:26 +0800

    lost files

---
+++ main.py
@@ -0,0 +1,4 @@
+from src.masks import get_mask_account, get_mask_card_number
+
+print(get_mask_card_number("1234567890987654"))
+print(get_mask_account("1234567890987654"))
---
+++ src/masks.py
@@ -0,0 +1,8 @@
+def get_mask_card_number(num_card: str) -> str:
+    """Принимает на вход номер карты и возвращает ее маску  XXXX XX** **** XXXX"""
+    return num_card[0:4] + " " + num_card[5:7] + "**" + " **** " + num_card[-4:]
+
+
+def get_mask_account(small_num_card: str) -> str:
+    """Принимает на вход номер карты и возвращает ее маску **XXXX"""
+    return "**" + small_num_card[-4:]
