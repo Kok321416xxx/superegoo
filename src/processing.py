66d65ba610c424ae98f76f75db539e3f65d2b003
Artem <kok321416x@yandex.ru>
2025-02-08 11:55:52 +0800

    procesig and main

--- src/processing.py
+++ src/processing.py
@@ -1,22 +1,16 @@
-from traceback import print_tb
-
-
-def filter_by_state(*dict_for_state, state="EXECUTED"):
+def filter_by_state(dict_for_state: list[dict] , state : str ="EXECUTED") -> list[dict]:
+    """Принимает список словарей и опционально значение для ключа state (по умолчанию 'EXECUTED' ). Функция возвращает новый список словарей, содержащий только те словари, у которых ключ state соответствует указанному значению."""
     filtered_list  = []
     for d in dict_for_state:
         if d.get('state') == state:
             filtered_list.append(d)
     return filtered_list
-#return [item for item in dict_for_state if item.get('state') == state]
 
 
-def sort_by_date(*dict_sort_by_date, reverse=True):
-        sorted_date = sorted(dict_sort_by_date, key='date', reverse=True)
+def sort_by_date(dict_sort_by_date: list[dict], reverse: bool=True) -> list[dict]:
+    """ Gринимает список словарей и необязательный параметр, задающий порядок сортировки (по умолчанию — убывание), взвращает новый список, отсортированный по дате (date)"""
+    return sorted(dict_sort_by_date, key= lambda x: x.get('date'), reverse=reverse)
 
-
-
-print(sort_by_date([{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'}, {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'}, {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'}, {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}]))
-print(filter_by_state({'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},{'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'}, {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'}, {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}))
 
 
 
