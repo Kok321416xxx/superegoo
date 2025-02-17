3da25f7137cb92acd3515369a241738ae527dce8
Artem <kok321416x@yandex.ru>
2025-02-17 20:33:07 +0800

    new HW 10_2 with tests

--- src/masks.py
+++ src/masks.py
@@ -1,8 +1,12 @@
 def get_mask_card_number(num_card: str) -> str:
+    if len(num_card) != 16:
+        raise ValueError('Некорректная длинна номера')
     """Принимает на вход номер карты и возвращает ее маску  XXXX XX** **** XXXX"""
-    return num_card[0:4] + " " + num_card[5:7] + "**" + " **** " + num_card[-4:]
+    return num_card[0:4] + " " + num_card[4:6] + "**" + " **** " + num_card[-4:]
 
 
 def get_mask_account(small_num_card: str) -> str:
+    if len(small_num_card) != 20:
+        raise ValueError('Некорректная длинна номера счета')
     """Принимает на вход номер карты и возвращает ее маску **XXXX"""
     return "**" + small_num_card[-4:]
